7f3c21ecdb3f469918a74b3c3f48820602f53404
wowi420 <nathanedwardkristanto@gmail.com>
2026-03-29 12:01:16 +0700

    File organizer v1

---
+++ file_organizer.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+import os
+import sys
+path = os.path.expanduser("~")
+# cwd= os.getcwd() <-- this will be used if i do make it inro argparse tool
+place = input("what dir you wanna organize:") #no need for parent dir
+standard_path = os.path.join(path,place)
+
+try:
+    files=(os.listdir(standard_path))
+except FileNotFoundError:
+    print(f"{place} does not exist")
+    sys.exit()
+os.chdir(standard_path)
+data = {".png":"pictures", ".jpg":"pictures", ".jpeg": "pictures", ".docx":"documents", ".pptx":"documents", ".txt" : "documents"}
+for file in files:
+    filet = file.lower()
+    for k, v in data.items():
+        if filet.endswith(k):
+            src= os.path.join(standard_path,file)
+            try:
+                Duplicate = False
+                counter = 1
+                for i in os.listdir(os.path.join(standard_path,v,)):
+                    if file == i:
+                        os.rename(src,os.path.join(standard_path,v, f"{file}-{counter}"))
+                        counter +=1
+                        Duplicate = True
+                        break
+                    else:
+                        pass
+                if not Duplicate:
+                    os.rename(src,os.path.join(standard_path,v,file))
+            except FileNotFoundError:
+                confirm = input(f"directories for {v} does not exist yet \n would you want to create a new directories?(y/n):")
+                if confirm == "y":
+                    os.mkdir(v)
+                    os.rename(src,os.path.join(standard_path,v,file))
+                    print("directories have been made")
+                else:
+                    continue
+print("The Process Is completed")
